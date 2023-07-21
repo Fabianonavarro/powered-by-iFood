becd34ee825240e6f89b6c3fb333ec06b686d549
Fabianonavarro <126501633+Fabianonavarro@users.noreply.github.com>
2023-07-21 15:31:59 -0300

    Add files via upload

---
+++ sistema_bancario.py
@@ -0,0 +1,90 @@
+'''
+Projeto sistema bancário
+
+Desenvolvido por Fabiano de Lima Navarro
+'''
+
+from datetime import date
+atual = date.today().year 
+print("___________________")
+print("Ano:",atual)
+print("====================")
+
+print("Seja bem vindo ao sistema bancário")
+
+menu = """
+---------------------------------
+Escolha as opções baixo
+
+Depositar     Opção [1]           
+Sacar         Opção [2]           
+Extrato       Opção [3]           
+Sair          Opção [s]           
+
+
+---------------------------------
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "s":
+        print("SAINDO")
+        
+        print("====")
+
+        print("FIM")
+              
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
